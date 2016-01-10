da50300c333b9c22a76cd17e75b36f985238864f
daimon-hiroaki <daimon-hiroaki@dmm.com>
2016-01-10 15:45:40 +0900

    ファイル削除

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ino44Api::Application.config.secret_token = 'ef85e26d840f77896f00676ccc795e8c7bf6943e1cc1a2bfb3899176d1a3e94e7d6072bc83ca82317b9de6f318e4005c9a69cd7505b238c2c6bf8da0fae7e266'
